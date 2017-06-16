7d685df10c7e2fb8d2e01982d44a08a04a713d38
Louise Camlin <lacamlin@gmail.com>
2017-06-16 13:59:04 +0100

    food spec tests pass

--- specs/food_spec.js
+++ specs/food_spec.js
@@ -1,10 +1,21 @@
 var Food = require('../food');
 var assert = require('assert');
 
-describe ('task', function(){
+describe ('food', function(){
 
   var food
 
+  beforeEach(function() {
+    food = new Food("pizza", 20); 
+  });
+
+  it("food should have a name", function() {
+    assert.strictEqual("pizza", food.name);
+  });
+
+  it("food should have replenishment value", function() {
+    assert.strictEqual(20, food.replenishmentValue)
+  });
 
 
 
@@ -16,4 +27,5 @@ describe ('task', function(){
 
 
 
-}
+
+});
