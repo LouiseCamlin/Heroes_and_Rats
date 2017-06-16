af0d00cfbd2e780c5cf3a208d8da50861ffb0dc6
Louise Camlin <lacamlin@gmail.com>
2017-06-16 13:49:25 +0100

    task.js and tests

---
+++ specs/task_spec.js
@@ -0,0 +1,33 @@
+var Task = require('../task');
+var assert = require('assert');
+
+describe ('task', function(){
+
+  var task;
+
+  beforeEach(function(){
+    task = new Task(100, 10, "10 gold and a party", false);
+  });
+
+  it("task should have a diffficulty", function() {
+    assert.strictEqual(100, task.difficulty);
+  });
+
+  it("task should have an urgency", function() {
+    assert.strictEqual(10, task.urgency);
+  });
+
+  it("task should have a reward", function() {
+    assert.strictEqual("10 gold and a party", task.reward);
+  });
+
+  it("task should have a status", function() {
+    assert.strictEqual(false, task.completedStatus);
+  })
+
+
+
+
+
+
+})
---
+++ task.js
@@ -0,0 +1,15 @@
+var Task = function(difficulty, urgency, reward, completedStatus) {
+  this.difficulty = difficulty;
+  this.urgency = urgency;
+  this.reward = reward;
+  this.completedStatus = completedStatus;
+};
+
+
+
+
+
+
+
+
+module.exports = Task;
